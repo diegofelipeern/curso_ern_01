2337061fa48fc72cbeb890cf1062fca687ad8a9b
diegofelipeern <diego.felipe@ern.com.mx>
2020-05-18 14:12:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-rfdmn1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-rfdmn1)
---
+++ index.js
@@ -0,0 +1,12 @@
+// Import stylesheets
+var $ = require("jquery")
+
+import 'bootstrap';
+import './style.scss';
+
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+$("#main").append("Hola");
