f36d864a3a42dae82b7bdf41257f0b7258469c2e
diegofelipeern <diego.felipe@ern.com.mx>
2020-05-21 12:27:24 -0500

    4 files modified

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import './style.scss';
 
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+/*const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;*/
 
 $("#main").append("Hola");
